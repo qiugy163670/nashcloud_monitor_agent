a5fc75a824923d975a065ebc9ccb1f721e375025
jiajunmei <jun.nashyun.com>
2021-08-04 15:14:53 +0800

    add struct for convert json log

--- src/tail/log.go
+++ src/tail/log.go
@@ -1,7 +1,28 @@
-package tail
+package main
 
-type log struct {
-	log    string
-	stream string
-	time   string
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Log struct {
+	Log    string `json:"log"`
+	Stream string `json:"stream"`
+	Time   string `json:"time"`
+}
+
+func Json2Struct() {
+	jsonStr := `
+{
+	"log": "[2021-08-04 05:53:50] \u001b[32minfo\u001b[39m: ⛓  Got new block 2715180(0xad391f2c1d47a5f68637de50a9be989fde0a6c6052bef48218f0b361e6aa2fcd)\n",
+	"stream": "stdout",
+	"time": "2021-08-04T05:53:50.751753157Z"
+}`
+	var log Log
+	json.Unmarshal([]byte(jsonStr), &log)
+	fmt.Println(log.Time)
+}
+
+func main() {
+	Json2Struct()
 }
--- src/tail/logTail.go
+++ src/tail/logTail.go
@@ -1,4 +1,4 @@
-package tail
+package main
 
 import (
 	"fmt"
